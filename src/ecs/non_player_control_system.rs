ed9229f0ac7b8171e7b91fc6b5ebed2bd9afe522
Peter Allin <peter@peca.dk>
2019-09-07 15:45:17 +0200

    Use arena size instead of hardcoded numbers in UFO control

--- src/ecs/non_player_control_system.rs
+++ src/ecs/non_player_control_system.rs
@@ -1,35 +1,37 @@
 use crate::ecs::{MovementKind, Position, Velocity};
-use specs::{ReadStorage, System, WriteStorage};
+use crate::Arena;
+use specs::{Read, ReadStorage, System, WriteStorage};
 
 pub struct NonPlayerControl;
 
-fn control_ufo(position: &Position, velocity: &mut Velocity) {
-    // TODO: Get the bounding rect from Specs, and have a rect for the position
-    if position.rect.left() <= 0.0 {
+fn control_ufo(position: &Position, velocity: &mut Velocity, arena: &Arena) {
+    let arena_rect = arena.0;
+    if position.rect.left() <= arena_rect.left() {
         velocity.x = velocity.x.abs();
-    } else if position.rect.right() > 800.0 {
+    } else if position.rect.right() > arena_rect.right() {
         velocity.x = -velocity.x.abs();
     }
 
-    if position.rect.top() <= 0.0 {
+    if position.rect.top() <= arena_rect.top() {
         velocity.y = velocity.y.abs();
-    } else if position.rect.bottom() >= 600.0 {
+    } else if position.rect.bottom() >= arena_rect.bottom() {
         velocity.y = -velocity.y.abs();
     }
 }
 
 impl<'a> System<'a> for NonPlayerControl {
     type SystemData = (
+        Read<'a, Arena>,
         ReadStorage<'a, MovementKind>,
         ReadStorage<'a, Position>,
         WriteStorage<'a, Velocity>,
     );
 
-    fn run(&mut self, (kind, pos, mut vel): Self::SystemData) {
+    fn run(&mut self, (arena, kind, pos, mut vel): Self::SystemData) {
         use specs::Join;
         for (kind, pos, vel) in (&kind, &pos, &mut vel).join() {
             match kind {
-                MovementKind::UFO => control_ufo(pos, vel),
+                MovementKind::UFO => control_ufo(pos, vel, &*arena),
                 MovementKind::Bullet => {}
             }
         }
